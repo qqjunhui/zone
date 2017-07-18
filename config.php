ebb3ffb778003468e106a33727bd25fc166995a4
941221 <626607098@qq.com>
2017-07-18 23:49:46 +0800

    钱钱

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ if(!defined("MVC")){
 }
 return array(
     "database"=>array(
-        "host"=>"localhost",
-        "user"=>"root",
-        "pass"=>"",
-        "database"=>"zone",
-        "port"=>"3306",
+        "host"=>"sqld.duapp.com",
+        "user"=>"5f0a27b668e646fb8041492e31187215",
+        "pass"=>"ef267724bce545818756ef18cc4c8e5b",
+        "database"=>"zwdFhbpqsHkYRXlNbiQh",
+        "port"=>"4050",
     )
 );
